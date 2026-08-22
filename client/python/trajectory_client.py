e0b9e46ea18675afaba49df0725adde0196857e6
MRD <desireddymohithreddy0925@gmail.com>
2026-08-22 10:13:39 +0530

    style: run ruff format to satisfy CI checks
    Signed-off-by: MRD <desireddymohithreddy0925@gmail.com>

--- client/python/trajectory_client.py
+++ client/python/trajectory_client.py
@@ -7,6 +7,8 @@ except ImportError:
 
     def init_backend(app_name: str = "trajectory-ir-local") -> None:
         raise ImportError("DBOS driver is not installed. Run `pip install .[dbos]`")
+
+
 from trajectory_ir.effects import requires_block_and_gate
 from trajectory_ir.resume.gate import make_gated_tool_call
 from trajectory_ir.runtime.log import NodeLog
